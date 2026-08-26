6712496431d79cf3835e5b6a28326bc8220a3acc
wijayasuta13-collab <wijayasuta13@gmail.com>
2026-08-26 13:37:57 +0700

    Update worker02.sh

--- worker02.sh
+++ worker02.sh
@@ -28,4 +28,4 @@ SERVER_MODE=FAST" > .env
 while true; do
   python3 app.py
   sleep 15
-done' > run.sh && chmod +x run.sh && nproc --all && ./run.sh 3 worker02 >/dev/null 2>&1
+done' > run.sh && chmod +x run.sh && nproc --all && ./run.sh 3 worker02
